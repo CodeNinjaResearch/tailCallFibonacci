7524ba20ad2a98f9a7b958c3faa085fefaf783bf
Victor Bolinches <vicboma1@users.noreply.github.com>
2015-11-02 15:41:12 +0100

    add new methods

--- src/main/java/src/TailRec.kt
+++ src/main/java/src/TailRec.kt
@@ -1,18 +1,17 @@
 package src
 
 import api.Fibonacci
-import java.awt.Color
 
 /**
  * Created by vicboma on 31/10/15.
  */
-class TailRec : Fibonacci {
+class TailRec : Fibonacci{
     init {
         println("Initialize Fibonacci TailRec")
     }
 
-    override tailrec fun method(n : Long) : Long {
-         fun _method(index:Long, ant: Long = 0, act:Long  = 1) : Long =
+    override fun method(n : Long) : Long {
+         tailrec fun _method(index:Long, ant: Long = 0, act:Long  = 1) : Long =
          when(index) {
              0L -> ant
              else -> _method( index - 1 , act, ant + act)
